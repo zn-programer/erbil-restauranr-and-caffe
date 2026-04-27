2887904ea4c67d3e3453b7de5ddbb8054e080161
Zaidh <zozo32888@gmail.com>
2026-04-27 14:08:27 +0300

    jobs

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,14 +4,14 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCC99FcUVRcG-zUlh4LsEMTN4RgWwunp5U",
-  authDomain: "erbil-jobs.firebaseapp.com",
-  databaseURL: "https://erbil-jobs-default-rtdb.firebaseio.com",
-  projectId: "erbil-jobs",
-  storageBucket: "erbil-jobs.firebasestorage.app",
-  messagingSenderId: "709783001048",
-  appId: "1:709783001048:web:87bb2636999cdc32a2c4ad",
-  measurementId: "G-MXBH4KLLQC"
+  apiKey: "AIzaSyDT2YQQZjUBrE0LPVA08ZKh3KgEAFOTJNU",
+  authDomain: "jobs-d4bd9.firebaseapp.com",
+  databaseURL: "https://jobs-d4bd9-default-rtdb.firebaseio.com",
+  projectId: "jobs-d4bd9",
+  storageBucket: "jobs-d4bd9.firebasestorage.app",
+  messagingSenderId: "612067822945",
+  appId: "1:612067822945:web:9cc5388d0b480ac1c64920",
+  measurementId: "G-E69X22F69W"
 };
 
 // 1. إعطاء اسم فريد جداً لهذا التطبيق (مثلاً "jobs_app") 
@@ -21,4 +21,4 @@ const app = !getApps().find(a => a.name === "jobs_app")
   : getApp("jobs_app");
 
 // 2. تمرير رابط قاعدة البيانات الخاص بالوظائف يدوياً لقطع الشك باليقين
-export const db = getDatabase(app, "https://erbil-jobs-default-rtdb.firebaseio.com");
+export const db = getDatabase(app, "https://jobs-d4bd9-default-rtdb.firebaseio.com");
